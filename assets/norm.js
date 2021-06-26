85afb85f75ae61b2ebc6d9f5f259a12a20e538c0
Thijs Boom <twboom@hotmail.nl>
2021-06-26 23:45:06 +0200

    Did more

---
+++ assets/norm.js
@@ -0,0 +1,35 @@
+const norm = []
+
+// Pass
+norm.pass = []
+
+norm.pass.push(
+    function(grades) {
+        if (lowestGrade(grades) >= 6) {
+            return true
+        } else { return false };
+    }
+);
+
+norm.pass.push(
+    function(grades) {
+        if (
+            lowestGrade(grades) >= 5 &&
+            gradeFrequency(grades, 5) <= 1
+        ) {
+            return true
+        } else { return false };
+    }
+);
+
+norm.pass.push(
+    function(grades) {
+        if(
+            lowestGrade(grades) >= 4 &&
+            gradeFrequency(grades, 4) <= 1 &&
+            !isCoreSubject(subjectFromGrades(4))
+        ) {
+
+        }
+    }
+)
